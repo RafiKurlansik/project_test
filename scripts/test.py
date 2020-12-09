2262106c8807f5516f890185deb3a767c78035c8
Joshua Cook <joshua.cook@koolHerc.lan>
2020-12-08 17:25:53 -0800

    Install with Download

--- scripts/test.py
+++ scripts/test.py
@@ -16,3 +16,20 @@ math.squared(4)
 # MAGIC token = dbutils.secrets.get(scope = <USERNAME>, key = <KEY>)
 # MAGIC
 # MAGIC %pip install git+https://$token@github.com/RafiKurlansik/project_test.git
+
+# COMMAND ----------
+
+# MAGIC %md ### Install via Download
+
+# COMMAND ----------
+
+# MAGIC %sh wget https://github.com/RafiKurlansik/project_test/blob/main/dist/my_package-0.1-py3-none-any.whl?raw=true
+
+# COMMAND ----------
+
+ dbutils.fs.mv("file:/databricks/driver/my_package-0.1-py3-none-any.whl?raw=true",
+               "dbfs:/dist/my_package-0.1-py3-none-any.whl")
+
+# COMMAND ----------
+
+# MAGIC %pip install /dist/joshuacook/my_package-0.1-py3-none-any.whl
