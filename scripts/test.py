11d3542b9d75c0d5ab63bbfbb2ef85d12fec025e
joshuacook <joshuacook>
2020-12-09 00:14:31 +0000

    install direct from GH

--- scripts/test.py
+++ scripts/test.py
@@ -1,17 +1,11 @@
 # Databricks notebook source
-# MAGIC %sh wget https://github.com/RafiKurlansik/project_test/blob/main/dist/my_package-0.1-py3-none-any.whl?raw=true
-
-# COMMAND ----------
-
- dbutils.fs.mv("file:/databricks/driver/my_package-0.1-py3-none-any.whl?raw=true",
-               "dbfs:/dist/my_package-0.1-py3-none-any.whl")
-
-# COMMAND ----------
-
-# MAGIC %pip install /dbfs/dist/my_package-0.1-py3-none-any.whl
+# MAGIC %pip install git+https://github.com/RafiKurlansik/project_test
 
 # COMMAND ----------
 
 from my_package import math
 
 math.squared(4)
+
+# COMMAND ----------
+
