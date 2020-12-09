791ddb359536f9fd1f4383a416dd1869f408d9f5
Joshua Cook <joshua.cook@koolHerc.lan>
2020-12-08 17:23:01 -0800

    install with token

--- scripts/test.py
+++ scripts/test.py
@@ -9,3 +9,10 @@ math.squared(4)
 
 # COMMAND ----------
 
+# MAGIC %md ### Install with Token
+
+# COMMAND ----------
+
+# MAGIC token = dbutils.secrets.get(scope = <USERNAME>, key = <KEY>)
+# MAGIC
+# MAGIC %pip install git+https://$token@github.com/RafiKurlansik/project_test.git
